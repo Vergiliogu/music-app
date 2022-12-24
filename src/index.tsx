366c3f900e084ff6276e14afea23dadf2765c4bf
Vergiliogu <vergiliopoleza@gmail.com>
2022-12-24 18:27:34 -0300

    refactor: put files into it folders

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client'
-import GlobalStyle from './globalStyles'
-import Router from './router';
+import GlobalStyle from './styles/globalStyles'
+import Router from './routes/router';
 
 const root = createRoot(document.getElementById('root') as HTMLElement)
 root.render(
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from 'presentation/containers/navbar';
 
 const Router = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
-				
+				 <Route path='*' element={<Navbar />}>
+
+				 </Route>
 			</Routes>
 		</BrowserRouter>
 	);
